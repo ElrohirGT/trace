03fe54b96433e7659ed75ee86962470236b4a5ea
ElrohirGT <elrohirgt@gmail.com>
2022-05-19 04:34:59 -0600

    added a debug panic

--- src/windows/practice_window.rs
+++ src/windows/practice_window.rs
@@ -93,7 +93,10 @@ fn get_random_app_paragraph() -> AppParagraph {
     let current_dir = std::env::current_dir().unwrap();
     let path = Path::new(&current_dir).join("database.json");
     let json = fs::read_to_string(path).expect("");
-    let paragraphs: Vec<AppParagraph> = serde_json::from_str(&json).unwrap();
+    let paragraphs: Vec<AppParagraph> =  match serde_json::from_str(&json){
+        Ok(p) => p,
+        Err(why) => panic!("{:?}", why)
+    };
     return paragraphs.choose(&mut rand::thread_rng()).unwrap().clone();
 }
 fn create_practice_window<B: 'static + Backend>(_: &mut State) -> Option<Window<B>> {
