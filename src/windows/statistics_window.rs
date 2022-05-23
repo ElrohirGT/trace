6e3b9faaf3dff3ba990afbcb8fefa4914d1fc90f
ElrohirGT <elrohirgt@gmail.com>
2022-05-23 02:05:04 -0600

    Fixed the graph not showing in all the screen

--- src/windows/statistics_window.rs
+++ src/windows/statistics_window.rs
@@ -73,7 +73,7 @@ fn construct_line_chart<B: Backend>(
                         .fg(Color::LightRed)
                         .add_modifier(Modifier::BOLD),
                 ))
-                .bounds([0.0, raw_data_length as f64])
+                .bounds([0.0, raw_data_length as f64 - 1.0])
                 .labels((0..raw_data_length).filter_map(filter).collect()),
         )
         .y_axis(
