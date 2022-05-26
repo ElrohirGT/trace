52842280e9059cefc425e9d70b9575fc30001a43
ElrohirGT <elrohirgt@gmail.com>
2022-05-26 07:44:31 -0600

    Updated the server related windows

--- src/windows/join_server_window.rs
+++ src/windows/join_server_window.rs
@@ -1,8 +1,26 @@
-use crate::windows;
+use crate::windows::*;
 use crate::State;
 use crate::Window;
+use crate::WindowCommand;
+use crossterm::event::KeyCode;
+use std::collections::HashMap;
+use std::rc::Rc;
 use tui::backend::Backend;
+use tui::Frame;
+
+fn mp_join_server_window<B: 'static + Backend>(_: Rc<State>) -> Box<dyn Fn(&mut Frame<B>)> {
+    Box::new(|f| {})
+}
 
 pub fn create_mp_join_server_window<B: 'static + Backend>(_: &mut State) -> Option<Window<B>> {
-    None
+    Some(Window {
+        ui: mp_join_server_window,
+        commands: HashMap::from([(
+            KeyCode::Esc,
+            WindowCommand {
+                activator_key: KeyCode::Esc,
+                action: Box::new(create_multiplayer_menu_window),
+            },
+        )]),
+    })
 }
